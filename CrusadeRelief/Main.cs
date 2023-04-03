4fefa9c7fe66799286123d70b97c1791693d79a4
810560476 <810560476@qq.com>
2023-04-03 21:32:35 +0800

    添加项目文件。

---
+++ CrusadeRelief/Main.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using UnityModManagerNet;
+
+namespace CrusadeRelief {
+	public static class Main {
+		public static bool enabled = false;
+		public static bool Load(UnityModManager.ModEntry modEntry) {
+			modEntry.OnToggle = OnToggle;
+			new Harmony(modEntry.Info.Id).PatchAll(Assembly.GetExecutingAssembly());
+			return true;
+		}
+		public static bool OnToggle(UnityModManager.ModEntry modEntry, bool state) {
+			enabled = state;
+			return true;
+		}
+	}
+}
---
+++ CrusadeRelief/TacticalCombatAIBrainController_Tick_Patch.cs
@@ -0,0 +1,42 @@
+﻿using HarmonyLib;
+using Kingmaker;
+using Kingmaker.Armies;
+using Kingmaker.Armies.TacticalCombat;
+using Kingmaker.Armies.TacticalCombat.Brain;
+using Kingmaker.Armies.TacticalCombat.Controllers;
+using Kingmaker.Armies.TacticalCombat.Data;
+using Kingmaker.EntitySystem.Entities;
+using Kingmaker.PubSubSystem;
+
+namespace CrusadeRelief {
+	[HarmonyPatch(typeof(TacticalCombatAIBrainController), nameof(TacticalCombatAIBrainController.Tick))]
+	internal static class TacticalCombatAIBrainController_Tick_Patch {
+		public static bool Prefix(TacticalCombatDecisionContext ___s_UnitContext, TacticalCombatAIBrainController __instance) {
+			if(!Main.enabled) {
+				return true;
+			}
+			TacticalCombatController tacticalCombat = Game.Instance.TacticalCombat;
+			TurnState turn = tacticalCombat.Data.Turn;
+			UnitEntityData unit = turn.Unit;
+			if(tacticalCombat.Data.IsBusy || unit == null || unit.IsDirectlyControllable || !unit.Commands.Empty || turn.StandardActionUsed) {
+				return false;
+			}
+			TacticalCombatArmyData tacticalData = unit.GetTacticalData();
+			if(tacticalData.Faction == Kingmaker.Armies.ArmyFaction.Crusaders) {
+				return true;
+			}
+			___s_UnitContext.InitUnit(unit);
+			TacticalCombatData data = Game.Instance.TacticalCombat.Data;
+			data.Turn.IsBonusMoraleTurn = true;
+			data.Turn.UsedActionsCount = unit.Blueprint.GetArmyData().MaxExtraActions;
+			data.Turn.UpdateStandardAction(true);
+			EventBus.RaiseEvent<ITacticalCombatSkipTurnHandler>(delegate (ITacticalCombatSkipTurnHandler h) {
+				h.HandleSkipTurnTurn(data.Turn.Unit);
+			}, true);
+			Game.Instance.TacticalCombat.TurnController.EndTurn(false);
+			___s_UnitContext.ReleaseUnit();
+			___s_UnitContext.ReleaseGroup();
+			return false;
+		}
+	}
+}
